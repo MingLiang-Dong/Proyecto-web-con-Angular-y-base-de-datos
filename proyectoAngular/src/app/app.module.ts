d8d467ea710cef2dea91a1d993dc28e7a6c7213c
Jimmy Merino <44029363+jimmymerino50@users.noreply.github.com>
2024-05-29 13:30:19 +0200

    nuevos cambios
    asdad

--- proyectoAngular/src/app/app.module.ts
+++ proyectoAngular/src/app/app.module.ts
@@ -22,6 +22,7 @@ const appRoutes: Routes = [
 
 
   { path: '', component: LoginComponent },
+  {path: 'registro', component:ResgistrarComponent},
   { path: 'Home', component: LibrosComponent },
 
 ];
