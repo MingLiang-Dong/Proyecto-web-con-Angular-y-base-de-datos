8e21d48b6ce6ab632afcc6b73d56cd503435009c
Jimmy_Merino <44029363+jimmymerino50@users.noreply.github.com>
2024-06-03 22:40:04 +0200

    baseculera
    asdas

--- basedatos/libreria.sql
+++ basedatos/libreria.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-05-2024 a las 12:26:36
+-- Servidor: localhost:3307
+-- Tiempo de generación: 03-06-2024 a las 22:39:01
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -38,7 +38,8 @@ CREATE TABLE `categorias` (
 
 INSERT INTO `categorias` (`id_categoria`, `n_categoria`) VALUES
 (1, 'romance'),
-(2, 'accion');
+(2, 'accion'),
+(3, 'comedia');
 
 -- --------------------------------------------------------
 
@@ -61,8 +62,13 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id_categoria`, `id_libro`, `n_libro`, `descripcion`, `imagen`, `stock`, `precio`) VALUES
-(1, 1, 'DONDE NO PUEDES ENCONTRARME', 'Un romance que florece entre las grietas de un corazón roto.\r\n\r\nGala está harta de seguir atrapada en un círculo vicioso de dependencia y toxicidad con su exnovio.\r\n\r\n \r\nNO CONCIBE UNA VIDA SIN ÉL\r\n\r\n \r\nEs por ello que decide dejarlo todo atrás y alejarse completamente de él, con la esperanza de poder sanar su corazón y evitar la tentación de volver a caer en esa destructiva relación.\r\n\r\n \r\nHuir donde no pueda hacerle daño.\r\n\r\n \r\nDONDE NO PUEDA ENCONTRARLA\r\n\r\n \r\nUna nueva vida en la que refugiarse y con la que enfrentarse a sus inseguridades, sus heridas, sus miedos y su inoportuna atracción hacia Gael, un chico que forma parte de su nueva rutina.\r\n\r\n \r\nCON QUIEN PODRÍA REDESCUBRIR EL AMOR\r\n\r\n \r\nPero Gala tiene claro que volver a abrir su corazón no entra en sus planes.\r\n\r\n \r\nElla sabe que lo que necesita es centrarse en sí misma.\r\n\r\n \r\nLIBERARSE DEL PASADO, APRENDER A AMARSE Y PERMITIRSE SER FELIZ SIN DEPENDER DE NADIE MÁS', 'https://imagessl6.casadellibro.com/a/l/s7/16/9788408287216.webp', 12, 16),
-(2, 2, 'MASACRE MATA EL UNIVERSO MARVEL MARVEL MUST-HAVE', '¿Qué ocurriría si Masacre decidiera asesinar a todos los héroes del Universo Marvel? ¿Te seguiría pareciendo tan divertido? Vas a flipar. De verdad.', 'https://imagessl9.casadellibro.com/a/l/s7/19/9788410512719.webp', 15, 15.2);
+(2, 1, 'EL REY DEL PERÚ, JUAN PEDRO COSANO', 'Rebelión, pasión y epopeya en la desconocida vida del conquistador Gonzalo Pizarro.', 'https://m.media-amazon.com/images/I/91ELLpTFysL._AC_UF894,1000_QL80_.jpg', 30, 18.9),
+(2, 2, 'MASACRE MATA EL UNIVERSO MARVEL MARVEL MUST-HAVE', '¿Qué ocurriría si Masacre decidiera asesinar a todos los héroes del Universo Marvel? ¿Te seguiría pareciendo tan divertido? Vas a flipar. De verdad.', 'https://imagessl9.casadellibro.com/a/l/s7/19/9788410512719.webp', 15, 15),
+(1, 3, 'Amor y Amistad, Jane Austen', 'El noble joven nos informó de que su nombre era Lindsay, aunque por razones particulares lo llamaré aquí Talbot.» He aquí una elocuente muestra de lo que Chesterton calificó como «una especie de elegancia del absurdo» al caracterizar los escritos juveniles de Jane Austen, de los que reunimos en este volumen –bajo el titulo de Amor y amistad- una completa selección por primera vez en español.', 'https://images-eu.ssl-images-amazon.com/images/I/51bGIavETDL._SX195_.jpg', 20, 15.99),
+(1, 4, 'La edad de la inocencia, Edith Wharton\r\n', 'La edad de la inocencia (título original inglés: The Age of Innocence) es una novela de Edith Wharton publicada en 1920 y que fue galardonada en 1921 con el Premio Pulitzer. La acción de la novela transcurre en la alta sociedad neoyorquina de la década de 1870.', 'https://m.media-amazon.com/images/I/51q3kQZxnjL._SX195_.jpg', 15, 9.99),
+(1, 5, 'Norte y Sur, Elizabeth Gaskell\r\n', 'A través de la historia de Margaret Hale, una joven del Sur de Inglaterra que por circunstancias familiares se ve obligada a trasladarse al Norte, a la ciudad industrial de Milton -un trasunto apenas disfrazado de Manchester-, Elizabeth Gaskell plasma de forma magistral los conflictos sociales y políticos derivados de la Revolución Industrial en la Inglaterra de mediados del siglo XIX.', 'https://images-na.ssl-images-amazon.com/images/I/516-03-Yx5L._SX210_.jpg', 17, 20),
+(3, 6, 'MALDITO KARMA, DAVID SAFIER\r\n', 'La presentadora de televisión Kim Lange está en el mejor momento de su carrera cuando sufre un accidente y muere aplastada por el lavabo de una estación espacial rusa. En el más allá, Kim se entera de que ha acumulado mal karma a lo largo de su vida: ha engañado a su marido, ha descuidado a su hija y ha amargado a cuantos la rodean.', 'https://imagessl2.casadellibro.com/a/l/s7/82/9788432228582.webp', 12, 15.9),
+(3, 7, 'ENHORABUENA POR TU FRACASO, ARTURO GONZALEZ CAMPOS', 'Enhorabuena por tu fracaso es un desnudo integral de Arturo  González-Campos, lo que no sabemos si dice mucho a su favor. Una oda a  lo genuino y una invitación a que te atrevas a persistir en lo que te  apasiona; a fracasar y fracasar y volver a intentarlo para, con toda  probabilidad, seguir aprendiendo a fracasar. Este es un relato (¿o unas  memorias?) sobre esos fracasos que, sólo a veces, se convierten en  exitos y que han permanecido en la mente de Arturo González-Campos  durante toda su vida.', 'https://imagessl7.casadellibro.com/a/l/s7/97/9788418051197.webp', 34, 17.95);
 
 -- --------------------------------------------------------
 
@@ -82,14 +88,12 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`user`, `password`, `rol`, `email`) VALUES
-('a', 'a', 'user', 'a@gmail.com'),
+('', '', 'user', ''),
 ('admin', '1234', 'admin', ''),
-('asda', '312313', 'user', 'patata@gmail.es'),
-('juan', 'juan1234', 'user', ''),
+('jimmy', 'jimmy12345', 'user', 'yimmy70@gmail.co'),
 ('ming', '1234', 'user', 'ming@gmail.com'),
-('patata', '12345', 'user', 'patata@gmail.es'),
-('qwdwqd', 'qwwww', 'user', 'qwdqww'),
-('w', '', 'user', '');
+('panda', '123456789', 'user', 'elpelon@gmail.co'),
+('prueba', 'prueba', 'user', 'prueba50@gmail.c');
 
 --
 -- Índices para tablas volcadas
@@ -122,7 +126,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
